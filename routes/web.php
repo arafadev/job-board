eaabfc8d8ecca12e25f797517c640b12cc64f1e9
Arafa Dev <arafa.dev@gmail.com>
2023-06-25 10:14:16 +0300

    jobs page: Tag component & job info

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobController::class)->only(['index']);
